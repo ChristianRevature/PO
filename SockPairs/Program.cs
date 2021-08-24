62660decc906efaaef8c951d05b0938a0b2bf625
ChristianRevature <christian.romero@revature.net>
2021-08-24 18:04:08 -0400

    adds solution to SockPairs

---
+++ SockPairs/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace SockPairs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pairinator pairinator = new Pairinator();
+
+            Pairinator.NumberOfSockTypes("ABDFKSADFJLASDKFJH");
+        }
+    }
+}
