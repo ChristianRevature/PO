d830d59d9d107f3ca0739475f9dc7634dd310059
ChristianRevature <christian.romero@revature.net>
2021-08-26 11:03:56 -0400

    updates the methods for the challenge

--- SockPairs/Pairinator.cs
+++ SockPairs/Pairinator.cs
@@ -17,20 +17,23 @@ namespace SockPairs
             set { _Pairs = value; }
         }
 
-       public  Pairinator() { }
+       public Pairinator() { }
 
 
         /// <summary>
         /// This is a method that assigns the number of sock pairs from the sockbasket to a class variable
         /// </summary>
         /// <param name="sockbasket">a string of characters that represent 'socks'</param>
-        public void CountPairs(string sockbasket)
+        public static void CountPairs(string sockbasket)
         {
+            Dictionary<char, int> sockpairs = new Dictionary<char, int>();
             // pass the input string to a char array
             char[] letter = sockbasket.ToCharArray(); 
             Array.Sort(letter);
+            Console.WriteLine(letter);
 
-            Dictionary<char, int> sockpairs = new Dictionary<char, int>();
+            //returns index numbers 
+
             // make X number of collections of each unique sock type
 
             //foreach(char x in letter)
@@ -41,16 +44,33 @@ namespace SockPairs
             ////for each unique socktype in the sockbasket, count the number of times they appear in the sockbasket 
             //foreach(char type in sockbasket)
             //{
-                
+
             //    sockpairs.Add( type,  count);
             //}
         }
 
+        /// <summary>
+        /// This is a method which returns the total number of socks in a sockbasket
+        /// </summary>
+        /// <param name="sockbasket"></param>
         public static void NumberOfSockTypes(string sockbasket)
         {
             int types = sockbasket.Distinct().Count();
             Console.WriteLine("The number of sock types in the sock basket is:" + types);
         }
 
+        /// <summary>
+        /// This is a method which returns all the unique socks in the sockbasket
+        /// </summary>
+        /// <param name="sockbasket">The string of characters that represents all socks</param>
+        public static void SockTypes(string sockbasket)
+        {
+            IEnumerable<char> typelist = sockbasket.Distinct();
+           Console.WriteLine("The sock types in the sockbasket are:");
+            foreach(char x in typelist)
+            {
+                Console.WriteLine(x);
+            }
+        }
     }//eoc
 }//eon
